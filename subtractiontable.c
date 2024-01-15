e40ac1167e924a1ee747854da15f4110cbf156a2
roobanai01 <154311125+roobanai01@users.noreply.github.com>
2024-01-15 16:40:59 +0530

    Subtraction table

---
+++ subtractiontable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter how many tables you want: ");
+scanf("%d",&a);
+printf("Subtraction table upto %d\n",a);
+for(b=1;b<=a;b++)
+{
+for(c=1;c<=20;c++)
+printf("%d-%d=%d\n",b,c,b-c);
+printf("***************\n");
+}
+return 0;
+}
