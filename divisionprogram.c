79e6a10795ac47037e23bc86369cd8df70d65221
roobanai01 <154311125+roobanai01@users.noreply.github.com>
2024-01-15 11:57:43 +0530

    Division program

---
+++ divisionprogram.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf ("Enter the value for A:");
+    scanf("%d",&a);
+    printf ("Enter the value for B:");
+    scanf("%d",&b);
+    c=a/b;
+    printf("The Divided Value of A and B is:%d", c);
+    return 0;
+}
