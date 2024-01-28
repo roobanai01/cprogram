70a4ce76832d05cde171db59fa518fd78fe1999a
roobanai01 <154311125+roobanai01@users.noreply.github.com>
2024-01-28 11:47:25 +0530

    find the squareroot of the number

---
+++ squareroot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    int b;
+    printf("Enter the value:");
+    scanf("%d",&a);
+    b=sqrt(a);
+    printf("Squareroot of %d is:%d",a,b);
+    return 0;
+}
